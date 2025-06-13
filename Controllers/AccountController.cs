dcb305822ad3caf1903873bf9616e6cfa326f6c7
Nick-Kingston12 <nicholaskingston10@gmail.com>
2025-06-13 12:07:28 +0200

    Login and Logout

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Moo_Arvelous_Coders.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public async Task<IActionResult> Login(string EmailAddress, string Password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(EmailAddress, Password, false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Logout"); // TEMP redirect to test logout
+            }
+
+            ViewBag.Message = "Invalid login";
+            return View();
+        }
+
+        // GET: /Account/Logout
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return View(); // Show your styled logout page
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using Moo_Arvelous_Coders.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
@@ -15,8 +17,14 @@ builder.Services.AddDbContext<MooArvelousDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+// Identity config
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -38,6 +46,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseAuthentication(); // <--- REQUIRED
 app.UseAuthorization();
 
 app.MapControllerRoute(
