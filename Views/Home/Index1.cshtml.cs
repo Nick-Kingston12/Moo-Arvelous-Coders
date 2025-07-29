b46a312349e08391d6ec38015c577e7be30d8aa7
Simoné <s223329401@mandela.ac.za>
2025-07-29 19:36:11 +0200

    Created registration page for farmer under View Create and connected to the register button

--- Views/Home/Index1.cshtml.cs
+++ Views/Home/Index1.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Moo_Arvelous_Coders.Views.Home
 {
     public class Index1Model : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/Index"); // This goes to Index.cshtml
+            }
+
+            return Page();
         }
     }
 }
