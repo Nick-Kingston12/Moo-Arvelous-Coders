79ec59723fc0e261ca1e503be2857b3041212136
Nick-Kingston12 <nicholaskingston10@gmail.com>
2025-08-04 17:13:23 +0200

    Added HerdsController, Create Herd view, model validation, and layout updates

---
+++ Controllers/FarmsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moo_Arvelous_Coders.Models; // Update this to your actual namespace
+using System.Collections.Generic;
+
+namespace Moo_Arvelous_Coders.Controllers
+{
+    public class FarmsController : Controller
+    {
+        // TEMPORARY in-memory list (until DB is set up)
+        private static List<Farm> _farms = new List<Farm>();
+
+        // GET: /Farms/
+        public IActionResult Index()
+        {
+            return View(_farms);
+        }
+
+        // GET: /Farms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Farms/Create
+        [HttpPost]
+        public IActionResult Create(Farm model)
+        {
+            if (ModelState.IsValid)
+            {
+                _farms.Add(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Controllers/HerdsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moo_Arvelous_Coders.Models; // Update this to your actual namespace
+using System.Collections.Generic;
+
+namespace Moo_Arvelous_Coders.Controllers
+{
+    public class HerdsController : Controller
+    {
+        // TEMPORARY in-memory list (until connected to DB)
+        private static List<Herd> _herds = new List<Herd>();
+
+        // GET: /Herds/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Herds/Create
+        [HttpPost]
+        public IActionResult Create(Herd model)
+        {
+            if (ModelState.IsValid)
+            {
+                _herds.Add(model);
+                return RedirectToAction("Create"); // Or redirect to a Herd list page
+            }
+
+            return View(model);
+        }
+    }
+}
+
