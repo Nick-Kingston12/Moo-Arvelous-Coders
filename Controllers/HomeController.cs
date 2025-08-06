dbbea95bc5dd8ce0e7ce951889c17b83fee0f7ef
Simoné <s223329401@mandela.ac.za>
2025-08-06 11:11:23 +0200

    Fixed up small details across pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Moo_Arvelous_Coders.Controllers
             return View();
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
